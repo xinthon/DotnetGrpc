014ce1d8034152b0e7d17ed53bc028950e014a64
Sinthon Seng <s.sinthon@cas.com.kh>
2024-06-01 08:35:30 +0700

    Add stream data

--- DotnetGrpcService.Client/Program.cs
+++ DotnetGrpcService.Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DotnetGrpcService.Client.Protos;
+using Grpc.Core;
 using Grpc.Net.Client;
 
 GrpcChannel channel = GrpcChannel
@@ -18,12 +19,27 @@ Console.WriteLine(greeterReply.Message);
 Customer.CustomerClient customerClient = new Customer
     .CustomerClient(channel);
 
-var customerReply = await customerClient.GetCustomersAsync(new GetCustomersQuery()
+//var customerReply = customerClient.GetCustomersStream(new GetCustomersStreamQuery()
+//{
+//    PageNumber = 1,
+//    PageSize = 50
+//});
+
+//Console.WriteLine(customerReply);
+
+
+var customerQuery = new GetCustomersStreamQuery()
 {
     PageNumber = 1,
     PageSize = 50
-});
+};
 
-Console.WriteLine(customerReply);
+using (var serverStream = customerClient.GetCustomersStream(customerQuery))
+{
+    await foreach (var response in serverStream.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine("Server stream response: " + response.Name);
+    }
+}
 Console.ReadLine();
 
--- DotnetGrpcService/Services/CustomerService.cs
+++ DotnetGrpcService/Services/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using Azure;
+using Dapper;
 using DotnetGrpcService.Common.Abstractions;
 using DotnetGrpcService.Protos;
 using Grpc.Core;
@@ -9,6 +10,7 @@ namespace DotnetGrpcService.Services
     {
         private readonly ILogger<CustomerService> _logger;
         private readonly IDatabaseFactory _factory;
+
         public CustomerService(ILogger<CustomerService> logger, IDatabaseFactory factory)
         {
             _logger = logger;
@@ -21,12 +23,12 @@ namespace DotnetGrpcService.Services
 
             var response = new CustomerList();
 
-            using (var connection = _factory.CreateConnection())
+            using(var connection = _factory.CreateConnection())
             {
                 IEnumerable<CustomerResponse> customers = await connection
                     .QueryAsync<CustomerResponse>("SELECT * FROM Customers");
 
-                foreach (CustomerResponse customer in customers)
+                foreach(CustomerResponse customer in customers)
                 {
                     response.Customers.Add(customer);
                 }
@@ -37,14 +39,36 @@ namespace DotnetGrpcService.Services
             return response;
         }
 
-        public override async Task<CustomerResponse> GetCustomerById(GetCustomerByIdQuery request, ServerCallContext context)
+        public override async Task GetCustomersStream(
+            GetCustomersStreamQuery request,
+            IServerStreamWriter<CustomerResponse> responseStream,
+            ServerCallContext context)
         {
-            using (var connection = _factory.CreateConnection())
+            using(var connection = _factory.CreateConnection())
+            {
+                IEnumerable<CustomerResponse> customers = await connection
+                    .QueryAsync<CustomerResponse>("SELECT * FROM Customers");
+
+                foreach(CustomerResponse customer in customers)
+                {
+                    await responseStream.WriteAsync(customer);
+                    await Task.Delay(1000);
+                }
+            }
+        }
+
+        public override async Task<CustomerResponse> GetCustomerById(
+            GetCustomerByIdQuery request,
+            ServerCallContext context)
+        {
+            using(var connection = _factory.CreateConnection())
             {
                 CustomerResponse? customer = await connection
-                     .QueryFirstOrDefaultAsync<CustomerResponse>("SELECT * FROM Customers WHERE CustomerId = @CustomerId", new { CustomerId = request.CustomerId });
+                     .QueryFirstOrDefaultAsync<CustomerResponse>(
+                         "SELECT * FROM Customers WHERE CustomerId = @CustomerId",
+                         new { request.CustomerId });
 
-                if (customer == null)
+                if(customer == null)
                 {
                     throw new Exception("Customer was not found");
                 }
@@ -53,11 +77,14 @@ namespace DotnetGrpcService.Services
             }
         }
 
-        public override async Task<CustomerResponse> CreateCustomer(CreateCustomerRequest request, ServerCallContext context)
+        public override async Task<CustomerResponse> CreateCustomer(
+            CreateCustomerRequest request,
+            ServerCallContext context)
         {
-            using (var connection = _factory.CreateConnection())
+            using(var connection = _factory.CreateConnection())
             {
-                await connection.ExecuteAsync("INSERT INTO Customers(CustomerId, Name, LastName, Email, Address) VALUES(@CustomerId, @Name, @LastName, @Email, @Address)");
+                await connection.ExecuteAsync(
+                    "INSERT INTO Customers(CustomerId, Name, LastName, Email, Address) VALUES(@CustomerId, @Name, @LastName, @Email, @Address)");
 
                 return new CustomerResponse()
                 {
@@ -70,34 +97,37 @@ namespace DotnetGrpcService.Services
             }
         }
 
-        public override async Task<CustomerResponse> DeleteCustomer(DeleteCustomerRequest request, ServerCallContext context)
+        public override async Task<CustomerResponse> DeleteCustomer(
+            DeleteCustomerRequest request,
+            ServerCallContext context)
         {
-            using (var connection = _factory.CreateConnection())
+            using(var connection = _factory.CreateConnection())
             {
-                CustomerResponse? customer = await connection.QueryFirstOrDefaultAsync<CustomerResponse>("SELECT * FROM Customers WHERE CustomerId = @CustomerId", new
-                {
-                    CustomerId = request.CustomerId
-                });
+                CustomerResponse? customer = await connection.QueryFirstOrDefaultAsync<CustomerResponse>(
+                    "SELECT * FROM Customers WHERE CustomerId = @CustomerId",
+                    new { request.CustomerId });
 
-                if (customer == null)
+                if(customer == null)
                 {
                     throw new Exception("Faild to create the customer");
                 }
 
-                await connection.ExecuteAsync("INSERT INTO Customers(CustomerId, Name, LastName, Email, Address) VALUES(@CustomerId, @Name, @LastName, @Email, @Address)");
+                await connection.ExecuteAsync(
+                    "INSERT INTO Customers(CustomerId, Name, LastName, Email, Address) VALUES(@CustomerId, @Name, @LastName, @Email, @Address)");
 
                 return customer;
             }
         }
 
-        public override async Task<CustomerResponse> UpdateCustomer(UpdateCustomerRequest request, ServerCallContext context)
+        public override async Task<CustomerResponse> UpdateCustomer(
+            UpdateCustomerRequest request,
+            ServerCallContext context)
         {
-            using (var connection = _factory.CreateConnection())
+            using(var connection = _factory.CreateConnection())
             {
-                await connection.ExecuteAsync("UPDATE Customers SET Name = @Name, LastName = @LastName, Email = @Email, Address = @Address WHERE CustomerId = @CustomerId", new
-                {
-                    CustomerId = request.CustomerId,
-                });
+                await connection.ExecuteAsync(
+                    "UPDATE Customers SET Name = @Name, LastName = @LastName, Email = @Email, Address = @Address WHERE CustomerId = @CustomerId",
+                    request);
 
                 return new CustomerResponse()
                 {
